95f17b3725fda2598875847c4691eb36be5bf1dd
Stuart Wheater <stuart.wheater@arjuna.com>
2015-02-14 21:49:24 +0000

    Updated tests

--- spreadsheet2jdbc/src/test/java/org/risbic/dbplugins/demo/test/spreadsheet2jdbc/dataflownodes/SimpleTest.java
+++ spreadsheet2jdbc/src/test/java/org/risbic/dbplugins/demo/test/spreadsheet2jdbc/dataflownodes/SimpleTest.java
@@ -7,17 +7,21 @@ package org.risbic.dbplugins.demo.test.spreadsheet2jdbc.dataflownodes;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.junit.Test;
 import org.risbic.dbplugins.demo.spreadsheet2jdbc.CompaniesHouseSpreadsheet2JDBCProcessor;
 import static org.junit.Assert.*;
 import com.arjuna.databroker.data.connector.ObserverDataConsumer;
-import com.arjuna.databroker.data.jee.DataFlowNodeLifeCycleControl;
+import com.arjuna.databroker.data.core.DataFlowNodeLifeCycleControl;
+import com.arjuna.dbutilities.testsupport.dataflownodes.lifecycle.TestJEEDataFlowNodeLifeCycleControl;
 
 public class SimpleTest
 {
     @Test
     public void simpleInvocation()
     {
+        DataFlowNodeLifeCycleControl dataFlowNodeLifeCycleControl = new TestJEEDataFlowNodeLifeCycleControl();
+
         String              name       = "XSSF Data Processor";
         Map<String, String> properties = new HashMap<String, String>();
         properties.put(CompaniesHouseSpreadsheet2JDBCProcessor.DATABASE_CONNECTIONURL_PROPNAME, "jdbc:postgresql://localhost:5432/databroker");
@@ -25,10 +29,12 @@ public class SimpleTest
         properties.put(CompaniesHouseSpreadsheet2JDBCProcessor.DATABASE_PASSWORD_PROPNAME, "password");
         CompaniesHouseSpreadsheet2JDBCProcessor xssfDataProcessor = new CompaniesHouseSpreadsheet2JDBCProcessor(name, properties);
 
-        DataFlowNodeLifeCycleControl.processCreatedDataFlowNode(xssfDataProcessor, null);
+        dataFlowNodeLifeCycleControl.completeCreationAndActivateDataFlowNode(UUID.randomUUID().toString(), xssfDataProcessor, null);
         
         File file = new File("Test01.xlsx");
 
         ((ObserverDataConsumer<File>) xssfDataProcessor.getDataConsumer(File.class)).consume(null, file);
+        
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(xssfDataProcessor);
     }
 }
