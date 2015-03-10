859b1866ca7fdc7d659f1d0e461e3e5b340df429
Stuart Wheater <stuart.wheater@arjuna.com>
2015-03-10 10:44:56 +0000

    Updated to Q7 demo

--- demo-plugin/src/main/java/org/risbic/dbplugins/demo/DemoDataFlowNodeFactoriesSetup.java
+++ demo-plugin/src/main/java/org/risbic/dbplugins/demo/DemoDataFlowNodeFactoriesSetup.java
@@ -5,15 +5,19 @@
 package org.risbic.dbplugins.demo;
 
 import java.util.Collections;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+
 import com.arjuna.databroker.data.DataFlowNodeFactory;
 import com.arjuna.databroker.data.DataFlowNodeFactoryInventory;
+
 import org.risbic.dbplugins.demo.spreadsheet2jdbc.CompaniesHouseSpreadsheet2JDBCProcessorFactory;
 import org.risbic.dbplugins.demo.spreadsheet2jdbc.LibrarySpreadsheet2JDBCProcessorFactory;
+import org.risbic.dbplugins.demo.spreadsheetmetadata.SpreadsheetMetadataExtractorProcessorFactory;
 
 @Startup
 @Singleton
@@ -24,9 +28,11 @@ public class DemoDataFlowNodeFactoriesSetup
     {
         DataFlowNodeFactory companiesHouseSpreadsheet2JDBCProcessorFactory = new CompaniesHouseSpreadsheet2JDBCProcessorFactory("Companies House Spreadsheet to JDBC Processor Factory", Collections.<String, String>emptyMap());
         DataFlowNodeFactory librarySpreadsheet2JDBCProcessorFactory        = new LibrarySpreadsheet2JDBCProcessorFactory("Library Spreadsheet to JDBC Processor Factory", Collections.<String, String>emptyMap());
+        DataFlowNodeFactory spreadsheetMetadataExtractorProcessorFactory   = new SpreadsheetMetadataExtractorProcessorFactory("Spreadsheet Metadata Extractor Processor Factory", Collections.<String, String>emptyMap());
 
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(companiesHouseSpreadsheet2JDBCProcessorFactory);
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(librarySpreadsheet2JDBCProcessorFactory);
+        _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(spreadsheetMetadataExtractorProcessorFactory);
     }
 
     @PreDestroy
@@ -34,8 +40,9 @@ public class DemoDataFlowNodeFactoriesSetup
     {
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Companies House Spreadsheet to JDBC Processor Factory");
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Library Spreadsheet to JDBC Processor Factory");
+        _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Spreadsheet Metadata Extractor Processor Factory");
     }
 
-    @EJB(lookup="java:global/databroker/control-core/DataFlowNodeFactoryInventory")
+    @EJB(lookup="java:global/databroker/data-core-jee/DataFlowNodeFactoryInventory")
     private DataFlowNodeFactoryInventory _dataFlowNodeFactoryInventory;
 }
