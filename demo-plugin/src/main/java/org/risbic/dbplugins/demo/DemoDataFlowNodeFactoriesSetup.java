4cba08d7e05807e6ca9cc26a0c28dec0e45bdf33
Stuart Wheater <stuart.wheater@arjuna.com>
2015-07-06 11:39:46 +0100

    Integrating 'JSON Metadata Extractors'

--- demo-plugin/src/main/java/org/risbic/dbplugins/demo/DemoDataFlowNodeFactoriesSetup.java
+++ demo-plugin/src/main/java/org/risbic/dbplugins/demo/DemoDataFlowNodeFactoriesSetup.java
@@ -5,7 +5,6 @@
 package org.risbic.dbplugins.demo;
 
 import java.util.Collections;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
@@ -17,6 +16,8 @@ import org.risbic.dbplugins.demo.spreadsheet2jdbc.CompaniesHouseSpreadsheet2JDBC
 import org.risbic.dbplugins.demo.spreadsheet2jdbc.LibrarySpreadsheet2JDBCProcessorFactory;
 import org.risbic.dbplugins.demo.spreadsheetmetadata.SpreadsheetMetadataExtractorProcessorFactory;
 import org.risbic.dbplugins.demo.spreadsheetmetadata.ExtraSpreadsheetMetadataExtractorProcessorFactory;
+import org.risbic.dbplugins.demo.jsonmetadata.JSONMetadataExtractorProcessorFactory;
+import org.risbic.dbplugins.demo.jsonmetadata.ExtraJSONMetadataExtractorProcessorFactory;
 
 @Startup
 @Singleton
@@ -29,11 +30,15 @@ public class DemoDataFlowNodeFactoriesSetup
         DataFlowNodeFactory librarySpreadsheet2JDBCProcessorFactory           = new LibrarySpreadsheet2JDBCProcessorFactory("Library Spreadsheet to JDBC Processor Factory", Collections.<String, String>emptyMap());
         DataFlowNodeFactory spreadsheetMetadataExtractorProcessorFactory      = new SpreadsheetMetadataExtractorProcessorFactory("Spreadsheet Metadata Extractor Processor Factory", Collections.<String, String>emptyMap());
         DataFlowNodeFactory extraSpreadsheetMetadataExtractorProcessorFactory = new ExtraSpreadsheetMetadataExtractorProcessorFactory("Extra Spreadsheet Metadata Extractor Processor Factory", Collections.<String, String>emptyMap());
+        DataFlowNodeFactory jsontMetadataExtractorProcessorFactory            = new JSONMetadataExtractorProcessorFactory("JSON Metadata Extractor Processor Factory", Collections.<String, String>emptyMap());
+        DataFlowNodeFactory extraJSONMetadataExtractorProcessorFactory        = new ExtraJSONMetadataExtractorProcessorFactory("Extra JSON Metadata Extractor Processor Factory", Collections.<String, String>emptyMap());
 
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(companiesHouseSpreadsheet2JDBCProcessorFactory);
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(librarySpreadsheet2JDBCProcessorFactory);
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(spreadsheetMetadataExtractorProcessorFactory);
         _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(extraSpreadsheetMetadataExtractorProcessorFactory);
+        _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(jsontMetadataExtractorProcessorFactory);
+        _dataFlowNodeFactoryInventory.addDataFlowNodeFactory(extraJSONMetadataExtractorProcessorFactory);
     }
 
     @PreDestroy
@@ -43,6 +48,8 @@ public class DemoDataFlowNodeFactoriesSetup
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Library Spreadsheet to JDBC Processor Factory");
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Spreadsheet Metadata Extractor Processor Factory");
         _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Extra Spreadsheet Metadata Extractor Processor Factory");
+        _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("JSON Metadata Extractor Processor Factory");
+        _dataFlowNodeFactoryInventory.removeDataFlowNodeFactory("Extra JSON Metadata Extractor Processor Factory");
     }
 
     @EJB(lookup="java:global/databroker/data-core-jee/DataFlowNodeFactoryInventory")
